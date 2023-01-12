f7e02311213c4f6a311c591493bb08701e204744
RaduDum <dumitriu_radu2003@yahoo.com>
2023-01-12 19:11:58 +0000

    update

---
+++ Scripts/pageControl.js
@@ -0,0 +1,85 @@
+
+{
+
+
+    var careerBtn = document.getElementById("careerBtn");
+    var projectsBtn = document.getElementById("projectsBtn");
+    var skillsBtn = document.getElementById("skillsBtn");
+
+    careerBtn.addEventListener('click', (e) => {
+        delete_all();
+        displayCareerPage();
+    });
+
+    projectsBtn.addEventListener('click', (e) => {
+        delete_all();
+        displayProjectsPage();
+    });
+
+    skillsBtn.addEventListener('click', (e) => {
+        delete_all();
+        displaySkillsPage();
+    });
+
+
+    function delete_all() {
+        alert('PP');
+    }
+
+    function displayCareerPage() {
+
+
+    }
+
+    function displayProjectsPage() {
+
+
+    }
+
+    function displayProjectsPage() {
+
+
+    }
+}
+
+
+
+// OnUIButtonPress >
+// 
+// HideAll
+// DisplayCurrentSelection
+//
+//
+
+
+
+//    var artti1 = document.getElementById("art1");
+//    var artti2 = document.getElementById("art2");
+//    var artti3 = document.getElementById("art3");
+//    var artti4 = document.getElementById("art4");
+//    artti1.style.visibility = 'hidden';
+//    artti2.style.visibility = 'hidden';
+//    artti3.style.visibility = 'hidden';
+//    artti4.style.visibility = 'hidden';
+//          function hide_all() {
+//        artti1.style.visibility = 'hidden';
+//          artti2.style.visibility = 'hidden';
+//          artti3.style.visibility = 'hidden';
+//          artti4.style.visibility = 'hidden';
+//          }
+//          function show_1() {
+//        hide_all();
+//              artti1.style.visibility = 'visible';
+//          }
+//          function show_2() {
+//        hide_all();
+//              artti2.style.visibility = 'visible';
+//          }
+//          function show_3() {
+//        hide_all();
+//              artti3.style.visibility = 'visible';
+//          }
+//          function show_4() {
+//        hide_all();
+//              artti4.style.visibility = 'visible';
+//          }
