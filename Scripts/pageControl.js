da7cad2433261df0bbc716aa47f295a14c1ceda0
RaduDum <dumitriu_radu2003@yahoo.com>
2023-01-16 12:52:53 +0000

    wip

--- Scripts/pageControl.js
+++ Scripts/pageControl.js
@@ -1,5 +1,12 @@
 
 {
+    const CONTENT_NAMES =
+    [
+        "career",
+        "education",
+        "projects",
+        "skills"
+    ];
 
 
     var careerBtn = document.getElementById("careerBtn");
@@ -7,90 +14,74 @@
     var projectsBtn = document.getElementById("projectsBtn");
     var skillsBtn = document.getElementById("skillsBtn");
 
-    careerBtn.addEventListener('click', (e) => {
-        delete_all();
-        displayCareerPage();
+    const TransitionDuration = 0;
+
+    let DisplayText = [];
+    let buttonLock = false;
+
+
+    fetch("./Data/Content/skills.html")
+        .then(r => {
+            r.text();
+        })
+        .then(t => {
+            DisplayText[i] = t;
+            alert(t);
+        });
+
+    for (let i = 0; i < CONTENT_NAMES.length; i++) {
+        DisplayText.push("");
+
+        fetch("./Data/Content/" + CONTENT_NAMES[i] + ".html")
+            .then(r => {
+                r.text();
+                alert(r);
+            })
+            .then(t => {
+                DisplayText[i] = t;
+                alert(t);
+            });
+    }
+
+    document.addEventListener('DOMContentLoaded', function ()
+    {
+        for (let i = 0; i < CONTENT_NAMES.length; i++) {
+            document.getElementById(CONTENT_NAMES[i] + "Btn").addEventListener('click', (e) => {
+
+                let textArea = document.getElementById("content-display");
+
+                if (!buttonLock)
+                {
+                    clear();
+                    displayContentPage(textArea, i);
+                    buttonLock = true;
+
+                    setTimeout(function () {
+                        buttonLock = false;
+                    }, TransitionDuration);
+                }
+            });
+
+        }
     });
 
-    educationBtn.addEventListener('click', (e) => {
-        delete_all();
-        displayEducationBtn();
-    });
-    
-    projectsBtn.addEventListener('click', (e) => {
-        delete_all();
-        displayProjectsPage();
-    });
-
-    skillsBtn.addEventListener('click', (e) => {
-        delete_all();
-        displaySkillsPage();
-    });
-
-
-    function delete_all() {
-        alert('PP');
-    }
-
-    function displayCareerPage() {
-
+    function displayContentPage(textArea, i) {
+        if (textArea.style.opacity == 0) /* Case: Not currently displaying anything in this text area */ {
+            document.body.style.height = "200vh";
+            textArea.innerHTML = DisplayText[i];
+            textArea.style.opacity = 1;
+        }
+        else  /* Case where area is already displayed some data */ {
+            textArea.style.opacity = 0;
+            setTimeout(function () {
+                textArea.innerHTML = DisplayText[i];
+                textArea.style.opacity = 1;
+            }, TransitionDuration / 2);
+        }
 
     }
 
-    function displayEducationBtn() {
-
-
+    function clear() {
     }
 
-    function displayProjectsPage() {
-
-
-    }
-
-    function displayProjectsPage() {
-
-
-    }
 }
-
-
-
-// OnUIButtonPress >
-// 
-// HideAll
-// DisplayCurrentSelection
-//
-//
-
-
-
-//    var artti1 = document.getElementById("art1");
-//    var artti2 = document.getElementById("art2");
-//    var artti3 = document.getElementById("art3");
-//    var artti4 = document.getElementById("art4");
-//    artti1.style.visibility = 'hidden';
-//    artti2.style.visibility = 'hidden';
-//    artti3.style.visibility = 'hidden';
-//    artti4.style.visibility = 'hidden';
-//          function hide_all() {
-//        artti1.style.visibility = 'hidden';
-//          artti2.style.visibility = 'hidden';
-//          artti3.style.visibility = 'hidden';
-//          artti4.style.visibility = 'hidden';
-//          }
-//          function show_1() {
-//        hide_all();
-//              artti1.style.visibility = 'visible';
-//          }
-//          function show_2() {
-//        hide_all();
-//              artti2.style.visibility = 'visible';
-//          }
-//          function show_3() {
-//        hide_all();
-//              artti3.style.visibility = 'visible';
-//          }
-//          function show_4() {
-//        hide_all();
-//              artti4.style.visibility = 'visible';
-//          }
