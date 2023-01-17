075067e82e588eaa7b27d5b53b5dcfbe49c19919
RaduDum <dumitriu_radu2003@yahoo.com>
2023-01-17 15:32:27 +0000

    Update pageControl.js

--- Scripts/pageControl.js
+++ Scripts/pageControl.js
@@ -24,14 +24,16 @@ window.addEventListener("load", () =>
             .then(r => r.text())
             .then(t => DisplayText[i] = t)
 
-        document.getElementById(CONTENT_NAMES[i] + "Btn").addEventListener('click', (e) => {
+        var hash = CONTENT_NAMES[i] + "Btn";
+
+        document.getElementById(hash).addEventListener('click', (e) => {
 
             let textArea = document.getElementById("content-display");
 
             if (!buttonLock)
             {
                 clear();
-                displayContentPage(textArea, i);
+                displayContentPage(hash, textArea, i);
                 buttonLock = true;
 
                 setTimeout(function () {
@@ -44,13 +46,15 @@ window.addEventListener("load", () =>
 });
 
 
-function displayContentPage(textArea, i) {
+function displayContentPage(hash, textArea, i) {
     if (textArea.style.opacity == 0) /* Case: Not currently displaying anything in this text area */ {
+        location.hash = "#" + hash;
         document.body.style.height = "200vh";
         textArea.innerHTML = DisplayText[i];
         textArea.style.opacity = 1;
     }
     else  /* Case where area is already displayed some data */ {
+        location.hash = "#" + hash;
         textArea.style.opacity = 0;
         setTimeout(function () {
             textArea.innerHTML = DisplayText[i];
